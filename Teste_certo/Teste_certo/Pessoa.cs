0b76d61e47d58d586b3208deed2fce6f127a3dc2
guillerme09 <gb2777370@gmail.com>
2026-06-22 17:33:05 -0300

    Add files via upload

---
+++ Teste_certo/Teste_certo/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_certo
+{
+    internal class Pessoa
+    {
+       public string nome;
+        public int cpf;
+        public int rg;
+    }
+}
